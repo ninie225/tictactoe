e0296ef542a93097b7c7d11d160a0431868c0ad9
ninie225 <marine.runavot@hotmail.fr>
2025-11-14 13:31:44 +0100

    Ajout fonction vérification du gagnant et texte sur le statut de la partie en cours

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,9 +4,18 @@ import { useState } from "react"
 function Board() {
     const [xIsNext, setXIsNext] = useState(true)
     const [squares, setSquares] = useState(Array(9).fill(null))
+    const winner = calculateWinner(squares)
+    let status
+    
+    if (winner) {
+        status = winner + " a gagné !"
+    }
+    else {
+        status = "Prochain tour : " + (xIsNext ? "X" : "O")
+    }
 
     function handleClick(i){
-        if (squares[i]) {
+        if (squares[i] || calculateWinner(squares)) {
             return;
         }
         const nextSquares= squares.slice()
@@ -19,8 +28,31 @@ function Board() {
         setSquares(nextSquares)
         setXIsNext(!xIsNext)
     }
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+            }
+        }
+        return null;
+    }
+
     return(
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
